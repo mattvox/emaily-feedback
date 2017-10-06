984eba6a50bfe19871c52582cfa930c96c9e0d11
mattvox <mattbliffert@icloud.com>
2017-10-05 22:25:08 -0400

    Finished auth flow.

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+require('dotenv').config({ path: './config/.env' })
+require('./models/User')
+require('./services/passport')
+
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+
+
+mongoose.connect(process.env.MONGO_URI)
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+app.use(cookieSession({
+  // 30 days in milliseconds
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [process.env.COOKIE_KEY],
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/auth-routes')(app)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT)
+app.listen(PORT, () => (
+  /* eslint-disable no-console */
+  console.log(`Listening on localhost:${PORT}`)
+))
